1e1e27406c00bcdd15f154bae76b6ffd53ea6e60
yohei_yayoi <93367004+yoheiyayoi@users.noreply.github.com>
2026-06-18 17:44:22 +0700

    style: improve homepage

--- apps/docs/src/app/(home)/page.tsx
+++ apps/docs/src/app/(home)/page.tsx
@@ -1,19 +1,30 @@
+import { Alert, AlertAction, AlertDescription, AlertTitle } from "@/registry/new-york-v4/ui/alert";
+import { Button } from "@/registry/new-york-v4/ui/button";
+import { InfoIcon } from "lucide-react";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col justify-center text-center flex-1 px-4 mx-auto">
-      <h1 className="text-3xl md:text-4xl font-bold mb-4">
-        shadcn/ui components for ntuniso projects
-      </h1>
-      <h3 className="text-xl md:text-2xl font-semibold mb-4">
-        อยู่ระหว่าง indev และจำเป็นต้องมี shadcn ถึงจะใช้ได้นะ :)
-      </h3>
-      <p>
-        <Link href="/docs" className="font-medium text-xl underline">
-          /docs
-        </Link>
-      </p>
+    <div className="">
+      <img
+        src="/nitc_banner.png"
+        alt="NITC Banner"
+        className="sm:w-full sm:mt-0 md:max-w-6xl md:mt-6 h-auto mb-4 md:rounded-lg shadow-md mx-auto"
+      />
+
+      <div className="container max-w-6xl px-4 py-6 mx-auto">
+        <h1 className="text-3xl md:text-4xl font-bold mb-4">Welcome to NITC Documentation</h1>
+        <p className="text-lg mb-6">
+          This documentation is for showcasing NITC-Shadcn components, made for NTUNISo (NTUN IT Solutions)
+          <br />
+          ps. NITC stands for "NTUN IT Components"
+        </p>
+
+        <Alert className="border-amber-200 bg-amber-100 text-amber-900">
+          <InfoIcon />
+          <AlertTitle>This is shadcn/ui version! if you looking for heroui version please go to (link)</AlertTitle>
+        </Alert>
+      </div>
     </div>
   );
 }
